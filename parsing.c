4d0c763e6135d8fcec075603193c918d3ac4ed1d
pranavachudha <pranavachudha@gmail.com>
2026-03-15 15:50:04 +0530

    V0.2-postfix-parser

--- parsing.c
+++ parsing.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /* If we are compiling on windows compile these functions */
 #ifdef _WIN32
 #include <string.h>
@@ -22,9 +23,23 @@ void add_history(char* unused){}
 
 #else
 #include <editline/readline.h>
+#include "mpc.h"
 #endif
 
 int main(int argc, char** argv) {
+	mpc_parser_t* Number = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr = mpc_new("expr");
+	mpc_parser_t* Lisps = mpc_new("lisps");
+
+	mpca_lang(MPCA_LANG_DEFAULT,
+			" 					     \
+			number : /-?[0-9]+/; 			     \
+			operator: '+' | '-' | '*' | '/'; 	     \
+			expr: <number> | '(' <operator> <expr>+ ')'; \
+			lisps: /^/ <operator> <expr>+ /$/; 	     \
+			",
+			Number, Operator, Expr, Lisps);
 
 	/* Print the version and the exit instructions */
 	puts("Lisps Version 0.0.0.0.1");
@@ -32,19 +47,31 @@ int main(int argc, char** argv) {
 
 	/* In a never ending loop */
 	while (1) {
-
+		
 		/* Output our prompt and also get the input */
 		char* input = readline("lisps> ");
+
+		if (strcmp(input, "exit") == 0) {
+			break;
+		}
 
 		/* Add input to history */
 		add_history(input);
 
-		/* Echo input back to user */
-		printf("Entered Text:  %s\n", input);
+		/* Attempt to Parse the user Input */
+		mpc_result_t r;
+		if (mpc_parse("<stdin>", input, Lisps, &r)) {
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else {
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
 
-		/* Free retrieved input */
+		
+		
 	}
 
+	mpc_cleanup(4, Number, Operator, Expr, Lisps);
 	return 0;
 }
-
